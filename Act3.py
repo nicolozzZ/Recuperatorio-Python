2d3626d615c76d93bda8a170e236b0b1dcb81d4b
nicolozzZ <135234760+nicolozzZ@users.noreply.github.com>
2024-11-22 12:26:59 -0300

    Create Act3.py

---
+++ Act3.py
@@ -0,0 +1,16 @@
+list = ["manzana", "banana", "pera", "manzana", "naranja", "pera", "pera"]
+
+def contarElementos(list):
+    dicc = {}
+    for i in range(len(list)):
+        for j in range (len(list[i])):
+            dicc.setdefault(list[i],len(list[i]))
+    return dicc
+    
+print(contarElementos(list))
+
+File = open('File.txt', 'w')
+File.write(contarElementos(list))
+File.close()
+if (os.path.exists('File.txt')):
+  print('el archivo se creo correctamente')
