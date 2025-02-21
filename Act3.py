bb894d6dc96bfba3aa294cef8b798865d83c7d75
nicolozzZ <135234760+nicolozzZ@users.noreply.github.com>
2025-02-21 10:10:09 +0800

    Update Act3.py
    corrigiendo el codigo
    final del codigo

--- Act3.py
+++ Act3.py
@@ -1,3 +1,4 @@
+import os
 list = ["manzana", "banana", "pera", "manzana", "naranja", "pera", "pera"]
 
 def contarElementos(list):
@@ -5,7 +6,7 @@ def contarElementos(list):
     for i in range(len(list)):
         for j in range (len(list[i])):
             dicc.setdefault(list[i],len(list[i]))
-    return dicc
+    return str(dicc)
     
 print(contarElementos(list))
 
